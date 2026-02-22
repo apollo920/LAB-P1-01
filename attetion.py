ec517eaf685191bee929f27722212fe6fa22c49d
Apollo Lopes Damasceno Sá <apollolds2@gmail.com>
2026-02-22 19:03:10 -0300

    criando função scaled_dot_product_attention para extrair, calcular e dividir scores

--- attetion.py
+++ attetion.py
@@ -5,3 +5,18 @@ def softmax(matrix: np.ndarray):
     exponentials = np.exp(shifted_values)
     row_sums = exponentials.sum(axis=1, keepdims=True)
     return exponentials / row_sums
+
+def scaled_dot_product_attention(
+    Q: np.ndarray,
+    K: np.ndarray,
+    V: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    d_k = K.shape[1]
+    scaling_factor = np.sqrt(d_k)
+
+    raw_scores = Q @ K.T
+    scaled_scores = raw_scores / scaling_factor
+    attention_weights = softmax(scaled_scores)
+    attention_output = attention_weights @ V
+
+    return attention_output, attention_weights
